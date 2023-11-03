b0b51d0ebf17d9f8d26da00ff35a2dc546b9c06f
Pierre Pivin <chillingd@macbook-pro-de-pierre.home>
2023-11-03 15:00:56 +0100

    3commit

--- src/Offre.js
+++ src/Offre.js
@@ -79,29 +79,113 @@ votre poste.</content></div>
                               <div class="note"><content> Préparation aux épreuves orales des concours des grandes écoles, entretien de motivation ou d’embauche,
 pitch, prise de parole devant 100 personnes : apprenez à <strong>communiquer pour convaincre </strong></content></div>                      
 
-                           
+  <div class="offre_content line_between">
+
+  <div class='home_offre column_items_center'>
+
+                              
+<div class="photo"></div>
+      
+      
+      
+</div>
+
+<div class="offre_text column_start">
+  <h2title>Concours d’entrée aux grandes écoles :</h2title>
+<h2under2></h2under2>
+<content1>
+En 2 à 4 séances individuelles, nous allons passer en revue
+le déroulement de l’entretien avec le jury de l’école de
+votre choix.<br/><br/>
+
+Nous allons préparer vos réponses, identifier vos points
+forts, mais aussi, simuler l’entretien pour vous mettre en
+confiance et vous entraîner.
+</content1>
+
+<Link to="/Methode">
+                <a id='bouton' >Prendre rendez-vous</a>
+                </Link>
+
+</div>
+
+
+    
+    </div> 
+
+    <div class="offre_content line_between">
+
+
+    <div class="offre_text column_start">
+  <h2title>Entretien d’embauche :</h2title>
+<h2under2></h2under2>
+<content1>
+Que ce soit pour un stage, un 1er job ou un changement de
+carrière, l’entretien de motivation est un exercice qui
+demande de la préparation et de l’entrainement.<br/><br/>
+
+Mettons ensemble des mots précis et exacts sur votre
+parcours, votre projet professionnel et votre envie d’être
+sélectionné(e).
+</content1>
+
+
+
+</div>
+
+<div class="offre_text column_start">
+  <h2title>Prise de parole :</h2title>
+<h2under2></h2under2>
+<content1>
+Pour faire de votre prochaine prise de parole un moment
+réussi, voire agréable, il s’avère efficace de se concentrer
+sur le message que vous allez porter.<br/><br/>
+
+SI le contenu de votre
+communication est en résonance avec les attentes du
+public, il y a de fortes chances que tout se passe bien.
+Apprenez les règles de base qui permettent de favoriser
+l’adhésion de votre audience.
+</content1>
+
+
+
+</div>
+
+
+</div>                        
                             
                             
 <div class="line_between">
 
 <div class="column_items_center">
-  <h1title>61 %</h1title>
+  <h1title>+ 600</h1title>
   <h2under2></h2under2>
-  <content1>Text test</content1>
+  <br/>
+  <h2title>Entretiens depuis
+2007</h2title>
 </div>
 <div class="column_items_center">
-  <h1title>61 %</h1title>
+  <h1title>98 %</h1title>
   <h2under2></h2under2>
-  <content1>Text test</content1>
+  <br/>
+  <h2title>de taux de réussite moyen</h2title>
 </div>
 <div class="column_items_center">
-  <h1title>61 %</h1title>
+  <h1title>100 %</h1title>
   <h2under2></h2under2>
-  <content1>Text test</content1>
+  <br/>
+  <h2title>de taux de satisfaction</h2title>
 </div>
 
 
 </div>
+<br/><br/><br/>
+<Link to="/Methode">
+                <a id='bouton' >Prendre rendez-vous</a>
+                </Link>
+
+
 
 
                              
@@ -115,8 +199,7 @@ pitch, prise de parole devant 100 personnes : apprenez à <strong>communiquer po
     
     </div>
 
-                        <br/>
-                        <br />
+                        
                         
                         
 
